4e82e58ca63500b14ea2b7e68e71d34be98521a2
matthew weintrub <heytrub@gmail.com>
2015-11-23 14:28:05 -0800

    first commit: init project

---
+++ mapsApp/mapsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mapsApp
+//
+//  Created by Matthew Weintrub on 11/23/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mapsApp/mapsApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mapsApp
+//
+//  Created by Matthew Weintrub on 11/23/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
