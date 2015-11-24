05b0493807f4de7f14a8ffe02547d6deedad3f3e
matthew weintrub <heytrub@gmail.com>
2015-11-23 17:43:59 -0800

    adds 3 gps location to the map and links them to a button

--- mapsApp/mapsApp/ViewController.m
+++ mapsApp/mapsApp/ViewController.m
@@ -50,4 +50,33 @@
             _mapView.mapType = MKMapTypeHybrid;
         }
 }
+- (IBAction)locationButtonPressed:(UIButton *)sender {
+    
+    NSString *locationTitle = sender.titleLabel.text;
+    
+    if([locationTitle isEqualToString:@"1"]) {
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake( 47.706267, -122.325811); // south lake union
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+        
+        [self.mapView setRegion:region animated:YES];
+        
+    }
+    
+    if([locationTitle isEqualToString:@"2"]) {
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6200893, -122.3297394); // capitol hill
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+        
+        [self.mapView setRegion:region animated:YES];
+    }
+    
+    if([locationTitle isEqualToString:@"3"]) {
+        CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(47.6100893,-122.3297394); // IKEA store haha.
+        MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(coordinate, 500.0, 500.0);
+        
+        [self.mapView setRegion:region animated:YES];
+    }
+    
+    
+ 
+}
 @end
